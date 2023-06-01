cc6485cb18628382a8a63ddd453f50ffb48766c6
ZainMughal329 <zainmughal4379329@gmail.com>
2023-06-01 16:43:00 +0500

    round button

---
+++ lib/ReUsable/Components/round_button.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+// import '../values/colors.dart';
+
+class RoundButton extends StatelessWidget {
+  final String title;
+  final Color color, textcolor,borderColor;
+  final VoidCallback onPress;
+  final bool loading;
+  const RoundButton(
+      {super.key,
+        required this.title,
+        required this.onPress,
+        this.textcolor = Colors.white,
+        this.borderColor = Colors.white,
+        this.color = Colors.green,
+        this.loading = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPress,
+      child: Container(
+        height: 54.h,
+        width: 280.w,
+        padding: EdgeInsets.all(10.w),
+        margin: EdgeInsets.only(bottom: 15.h),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(5),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              spreadRadius: 1,
+              blurRadius: 2,
+              offset: const Offset(0, 2),
+            ),
+          ],
+        ),
+        child: Center(
+            child: loading
+                ? CircularProgressIndicator(
+              color: Colors.white,
+            )
+                : Text(
+              title,
+              style: Theme.of(context)
+                  .textTheme
+                  .subtitle2!
+                  .copyWith(fontSize: 16, color: textcolor),
+            )),
+      ),
+    );
+  }
+}
