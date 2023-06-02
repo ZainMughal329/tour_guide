d289d2bbe6073a732ba3653f4f27e5dd8dda062b
ZainMughal329 <zainmughal4379329@gmail.com>
2023-06-02 16:06:30 +0500

    make some changes in signin controller and added splash screen logic, it is not working fine, but I ll make it work soon,,,,

--- lib/pages/sessionPages/splashScreen/bindings.dart
+++ lib/pages/sessionPages/splashScreen/bindings.dart
@@ -1,13 +1,14 @@
-// import 'package:air_proj_comp/pages/sessionPages/splashScreen/controller.dart';
-// import 'package:air_proj_comp/pages/sessionPages/welcome/controller.dart';
-// import 'package:get/get.dart';
-//
-// // import '../../welcome/controller.dart';
-//
-// class SplashScreenBinding implements Bindings {
-//   @override
-//   void dependencies() {
-//     Get.lazyPut<SplashScreenController> ( () => SplashScreenController());
-//   }
-//
-// }
+
+import 'package:get/get.dart';
+
+import 'index.dart';
+
+// import '../../welcome/controller.dart';
+
+class SplashScreenBinding implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<SplashScreenController> ( () => SplashScreenController());
+  }
+
+}
--- lib/pages/sessionPages/splashScreen/controller.dart
+++ lib/pages/sessionPages/splashScreen/controller.dart
@@ -1,18 +1,17 @@
-// import 'dart:async';
-//
-// import '../../../../../tour_guide/lib/ReUsable/routes/names.dart';
-// import 'package:air_proj_comp/common/store/prefrencesStorage.dart';
-// import 'package:air_proj_comp/pages/sessionPages/splashScreen/state.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:get/get.dart';
-//
-// class SplashScreenController extends GetxController{
-// SplashScreenState state = SplashScreenState();
-// SplashScreenController();
-//
-// @override
-//   void onInit() {
-//   // TODO: implement onReady
-//   super.onInit();
-//
-// }}
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+import 'index.dart';
+
+class SplashScreenController extends GetxController{
+SplashScreenState state = SplashScreenState();
+SplashScreenController();
+
+@override
+  void onInit() {
+  // TODO: implement onReady
+  super.onInit();
+
+}}
